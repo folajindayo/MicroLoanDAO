5fb98a99b462feaa26a1b9758f691438624e57c6
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:19:33 +0100

    Updated src/components/ui/LoadingSkeleton.tsx

---
+++ src/components/ui/LoadingSkeleton.tsx
@@ -0,0 +1,12 @@
+export default function LoadingSkeleton() {
+  return (
+    <div className="animate-pulse space-y-4">
+      <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-3/4"></div>
+      <div className="space-y-3">
+        <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded"></div>
+        <div className="h-4 bg-gray-200 dark:bg-gray-700 rounded w-5/6"></div>
+      </div>
+    </div>
+  )
+}
+
