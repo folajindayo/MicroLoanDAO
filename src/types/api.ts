bb7fe91e30a7a9f53b2350b1f56916367a353d44
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 21:07:03 +0100

    Updated src/types/api.ts

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,4 +1,10 @@
+import { Loan } from './loan'
+import { User } from './user'
+
 export interface ApiResponse<T> {
-  data?: T
-  error?: string
+    data?: T
+    error?: string
 }
+
+export type LoansResponse = Loan[]
+export type UserHistoryResponse = User
