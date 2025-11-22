331981b9e27336eb1a60c3e94bd249634d50c91e
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:24:50 +0100

    Updated src/hooks/useCurrencyFormatter.ts

--- src/hooks/useCurrencyFormatter.ts
+++ src/hooks/useCurrencyFormatter.ts
@@ -1,13 +1,12 @@
 import { formatEther } from 'viem'
 
 export function useCurrencyFormatter() {
-    const formatETH = (value: string | bigint) => {
-        try {
-            return `${formatEther(BigInt(value))} ETH`
-        } catch {
-            return '0 ETH'
-        }
+  const formatETH = (value: string | bigint) => {
+    try {
+      return `${formatEther(BigInt(value))} ETH`
+    } catch {
+      return '0 ETH'
     }
-    return { formatETH }
+  }
+  return { formatETH }
 }
-
