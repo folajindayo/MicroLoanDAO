e6396c548a25a3a541803bde82ebf83ff6ea00f2
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:35:56 +0100

    Updated jest.setup.ts

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,6 @@
 import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
 
+global.TextEncoder = TextEncoder
+// @ts-expect-error TextDecoder is not part of global type
+global.TextDecoder = TextDecoder
