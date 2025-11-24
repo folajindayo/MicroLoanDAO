d77cba71fd5752f074b94b8c2ec469798879e184
folajindayo <temidayofolajin@yahoo.com>
2025-11-24 20:59:27 +0100

    docs: add JSDoc to User interface

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,8 +1,15 @@
 import { Loan } from './loan'
 
+/**
+ * Represents a user in the system.
+ */
 export interface User {
+    /** Wallet address of the user */
     address: string
+    /** Calculated reputation score based on loan history */
     reputationScore: number
+    /** History of loans requested by this user */
     loans: Loan[]
+    /** History of loans funded by this user */
     fundedLoans: Loan[]
 }
