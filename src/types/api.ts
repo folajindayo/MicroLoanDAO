63d17c56b7aeb2ff42214adb7208d3413f6880a4
folajindayo <temidayofolajin@yahoo.com>
2025-11-24 20:59:27 +0100

    docs: add JSDoc to API types

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,10 +1,18 @@
 import { Loan } from './loan'
 import { User } from './user'
 
+/**
+ * Generic API response wrapper.
+ */
 export interface ApiResponse<T> {
+    /** The data returned by the API */
     data?: T
+    /** Error message if the request failed */
     error?: string
 }
 
+/** Response type for fetching a list of loans */
 export type LoansResponse = Loan[]
+
+/** Response type for fetching user history */
 export type UserHistoryResponse = User
