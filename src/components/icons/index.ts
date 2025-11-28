f91c5975da8231e2932d03acf70f0f070d6b44f0
folajindayo <temidayofolajin@yahoo.com>
2025-11-28 15:15:57 +0100

    Create icons directory with barrel export

---
+++ src/components/icons/index.ts
@@ -0,0 +1,11 @@
+// Icon components barrel export
+export * from './WalletIcon';
+export * from './CoinIcon';
+export * from './ChartIcon';
+export * from './ClockIcon';
+export * from './CheckIcon';
+export * from './XIcon';
+export * from './ArrowIcon';
+export * from './UserIcon';
+export * from './StarIcon';
+
